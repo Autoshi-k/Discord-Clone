a69ac4c227fddd644d0d16ffaaf910305cc2075f
Autoshi-k <autoshigit@gmail.com>
2022-01-17 17:46:48 +0200

    comment

--- server/routes/channels.js
+++ server/routes/channels.js
@@ -4,6 +4,9 @@ import User from '../models/User.js';
 
 export const router = express.Router();
 
+// the whole app in under url /channels therefor this /api/channels
+// is the first stop after logging in
+// getting user information is the only purpose 
 router.get('/', verify, async (req, res) => {
   const user = await User.find({ id: req.user.id }, { password: 0 });
   // sends back all the information about the user (password not included)
