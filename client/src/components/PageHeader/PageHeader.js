dfd589f16f88e5ea3ed3bbe3733c1961e6f5607e
Autoshi-k <autoshigit@gmail.com>
2022-02-01 13:05:02 +0200

    friends header update css

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -4,7 +4,8 @@ import GroupsIcon from '@mui/icons-material/Groups';
 
 import { Divider } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeLocation } from '../../features/location';
 import StatusIcon from '../../Utilities/StatusIcon';
 
 
@@ -13,6 +14,8 @@ function PageHeader() {
   const rooms = useSelector(state => state.rooms.value);
   const location = useSelector(state => state.location.value);
 
+  const dispatch = useDispatch();
+
   const [userToDisplay, setUserToDisplay] = useState(null);
   useEffect(() => {
     if (location.room === 'friends') return;
@@ -20,38 +23,40 @@ function PageHeader() {
     const test =  Object.keys(currentRoom.participants).filter(participant => currentRoom.participants[participant]._id !== user._id)
     setUserToDisplay(rooms[location.room].participants[test]);
   }, [rooms, location])
-
+  const friendsListItems = ['online', 'all', 'pending', 'blocked', 'add-friend'];
   return (
     <>
       <div className="header">
-        
-        {
-            location.room === 'friends' ?
-            <>
-              <div className='friends-header'>            
-                <div aria-hidden="true" className="chat-header-at-icon"><GroupsIcon fontSize='large' sx={{ fontSize: 30, color: '#B9BBBE', width: '2rem' }}/> </div>
-                <div className='room-name'>Friends</div>
-              </div>
-              <ul className='friends-header-list'>
-                <li className={location.subRoom === 'online' ? 'active' : null}>online</li>
-                <li className={location.subRoom === 'all' ? 'active' : null}>all</li>
-                <li className={location.subRoom === 'pending' ? 'active' : null}>pending</li>
-                <li className={location.subRoom === 'blocked' ? 'active' : null}>blocked</li>
-                <li className={`add-friend ${location.subRoom === 'blocked' ? 'active' : null}`}>add friend</li>
-              </ul>
-            </>
-            :
-            <>
-              <div aria-hidden="true" className="chat-header-at-icon"><AlternateEmailIcon sx={{ fontSize: 30, color: '#B9BBBE', width: '2rem' }} /></div>
-              <div className='room-name'>{ userToDisplay ? userToDisplay.displayName : '' }</div>
-              <StatusIcon
-                currentStatus={ userToDisplay?.currentStatus }
-              alt={ 'status-' }
-              />
-            </>    
-        }
-        
-        </div>
+      {
+        location.room === 'friends' ?
+        <>
+          <div className='friends-header'>            
+            <div aria-hidden="true" className="chat-header-at-icon"><GroupsIcon fontSize='large' sx={{ fontSize: 30, color: '#B9BBBE', width: '2rem' }}/> </div>
+            <div className='room-name'>Friends</div>
+          </div>
+          <ul className='friends-header-list'>
+            { friendsListItems.map(item => {
+              return (
+              <li 
+                className={`${location.subRoom === item ? 'active' : null} ${item === 'add-friend' ? 'add-friend' : null}`}
+                onClick={ () => dispatch(changeLocation({ ...location, subRoom: item })) }
+                >
+                { item.split('-').join(' ') }
+              </li>
+              )}) }
+          </ul>
+        </>
+        :
+        <>
+          <div aria-hidden="true" className="chat-header-at-icon"><AlternateEmailIcon sx={{ fontSize: 30, color: '#B9BBBE', width: '2rem' }} /></div>
+          <div className='room-name'>{ userToDisplay ? userToDisplay.displayName : '' }</div>
+          <StatusIcon
+            currentStatus={ userToDisplay?.currentStatus }
+          alt={ 'status-' }
+          />
+        </>    
+      }
+      </div>
       <Divider />
     </>
   )
