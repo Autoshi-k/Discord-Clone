5bd0f8da086cc6329744f26b89908366adac9163
Autoshi-k <autoshigit@gmail.com>
2022-02-07 16:30:03 +0200

    socket not up to date, new db

--- server/index.js
+++ server/index.js
@@ -15,11 +15,13 @@ import { router as channelsRouter } from './routes/channels.js';
 import { router as findUsersRouter } from './routes/users.js';
 import Message from './models/Message.js';
 import Participant from './models/Participant.js';
-import User from './models/User.js';
+// import User from './models/User.js';
+import db from './connection.js';
 
 dotenv.config();
 
 // Connect to DB
+// app.use(db);
 mongoose.connect('mongodb://localhost:27017/discordDB', () => console.log('connected to DB!'));
 
 // MiddleWares
@@ -40,51 +42,55 @@ const io = new Server(server);
 
 io.on("connection", async socket => {
   
-  // at connection getting information about the user
-  // and joining all the relevate rooms
-  const user = await User.findById(socket.handshake.auth.userId);
-  const RoomsAsObj = await Participant.find({ userId: socket.handshake.auth.userId })
-  let rooms = RoomsAsObj.map(room => room.roomId);
-  socket.join(rooms);
+  // // at connection getting information about the user
+  // // and joining all the relevate rooms
+  // // const user = await User.findById(socket.handshake.auth.userId);
+  // // const RoomsAsObj = await Participant.find({ userId: socket.handshake.auth.userId })
+  // // let rooms = RoomsAsObj.map(room => room.roomId);
+  // // socket.join(rooms);
 
-  // user making a new conversation
-  socket.on('add room', newRoom => {
-    rooms.push(newRoom.roomId);
-    socket.join(newRoom);
+  socket.on('add friend pending', pendingRequest => {
+    
   })
+
+  // // user making a new conversation
+  // socket.on('add room', newRoom => {
+  //   rooms.push(newRoom.roomId);
+  //   socket.join(newRoom);
+  // })
   
-  // user change his status and update all rooms
-  socket.on('change my status', async statusNumber => {
-    user.currentStatus = statusNumber;
-    await user.save();
-    socket.to(rooms).emit('user changed status', { userId: user._id, newStatus: statusNumber })
-    // WHEN DO I NEED TO ADD .TOSTRING() ??????????? user._id ---> hjfwhr374236247
-    // new ObjectId("jkhdfhfu38e3824")
-  })
+  // // user change his status and update all rooms
+  // socket.on('change my status', async statusNumber => {
+  //   user.currentStatus = statusNumber;
+  //   await user.save();
+  //   socket.to(rooms).emit('user changed status', { userId: user._id, newStatus: statusNumber })
+  //   // WHEN DO I NEED TO ADD .TOSTRING() ??????????? user._id ---> hjfwhr374236247
+  //   // new ObjectId("jkhdfhfu38e3824")
+  // })
 
-  socket.on('try send new message', async ({ message, to }) => {
-    // to = roomId
-    const participantId = await Participant.findOne({ userId: user._id.toString(), roomId: to }).select('_id');
-    const newMessage = new Message({
-      participantId: participantId._id.toString(),
-      content: message
-      });
-    try {
-      await newMessage.save();
-    } catch (err) { console.log(err) }
+  // socket.on('try send new message', async ({ message, to }) => {
+  //   // to = roomId
+  //   const participantId = await Participant.findOne({ userId: user._id.toString(), roomId: to }).select('_id');
+  //   const newMessage = new Message({
+  //     participantId: participantId._id.toString(),
+  //     content: message
+  //     });
+  //   try {
+  //     await newMessage.save();
+  //   } catch (err) { console.log(err) }
 
-    socket.to(to).emit('success send new message', { roomId: to, newMessage });
-  })
+  //   socket.to(to).emit('success send new message', { roomId: to, newMessage });
+  // })
   
-  socket.on('disconnect', async () => {
-    // user.currentStatus = 0; // still havnt decide if idle or disconnected
-    // try {
-    //   await user.save();
-    // } catch (err) { console.log(err) }
+  // socket.on('disconnect', async () => {
+  //   // user.currentStatus = 0; // still havnt decide if idle or disconnected
+  //   // try {
+  //   //   await user.save();
+  //   // } catch (err) { console.log(err) }
 
-    // this thing up here is messing up my code
-    // need to make it a normal on/emit
-  });
+  //   // this thing up here is messing up my code
+  //   // need to make it a normal on/emit
+  // });
 });
 
 server.listen(3001, (err) => console.log(err ? err : 'IS OKE'));
