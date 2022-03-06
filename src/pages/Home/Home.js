884e4b6a2c818a4ad5ae8866f76702794ba486a5
Autoshi-k <autoshigit@gmail.com>
2022-03-06 13:58:55 +0200

    redirecting if user connected

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,33 +1,39 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import clouds from '../../assets/clouds.svg';
 import leftArt from '../../assets/left.svg';
 import rightArt from '../../assets/right.svg';
 
 import './Home.css';
 const Home = () => {
-
+  const [redirect, setRedirect] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem('auth-token')) setRedirect(true);
+  }, []);
   return (
-    <div className="home">
-      {/* hello world */}
-      <div className="cover-text">
-        <h1>IMAGINE A PLACE...</h1>
-        <p>
-          ...where you can belong to a school club, a gaming group, or a worldwide art community. 
-          Where just you and a handful of friends can spend time together. 
-          A place that makes it easy to talk every day and hang out more often.
-        </p>
-        <div className='home-buttons-container'>
-          <Link to="/register"><div className='home-button register'>join discord now</div></Link>
-          <Link to="/login"><div className='home-button login'>already have a user?</div></Link>
-        </div>
+    <>
+      { redirect && <Navigate to="/channels" /> }
+      <div className="home">
+        <div className="cover-text">
+          <h1>IMAGINE A PLACE...</h1>
+          <p>
+            ...where you can belong to a school club, a gaming group, or a worldwide art community. 
+            Where just you and a handful of friends can spend time together. 
+            A place that makes it easy to talk every day and hang out more often.
+          </p>
+          <div className='home-buttons-container'>
+            <Link to="/register"><div className='home-button register'>join discord now</div></Link>
+            <Link to="/login"><div className='home-button login'>already have a user?</div></Link>
+          </div>
 
+        </div>
+        <div aria-hidden="true" className="cover-background">
+          <img src={ clouds } className="cover" alt="clouds" />
+          <img src={ leftArt } className="cover" alt="left" />
+          <img src={ rightArt } className="cover right" alt="right" />
+        </div>
       </div>
-      <div aria-hidden="true" className="cover-background">
-        <img src={ clouds } className="cover" alt="clouds" />
-        <img src={ leftArt } className="cover" alt="left" />
-        <img src={ rightArt } className="cover right" alt="right" />
-      </div>
-    </div>
+    </>
   )
 }
 
