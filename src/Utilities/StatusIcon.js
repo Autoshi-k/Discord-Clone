cb551034eb6839a8760f7f31ad3d6653d1928331
Autoshi-k <autoshigit@gmail.com>
2022-03-06 13:58:02 +0200

    now status is styled div and not image

--- src/Utilities/StatusIcon.js
+++ src/Utilities/StatusIcon.js
@@ -1,21 +1,7 @@
 import { Avatar, Badge } from "@mui/material";
-import { styled } from '@mui/material/styles';
-
-import offline from '../assets/offline.png';
-import online from '../assets/online.png';
-import idle from '../assets/idle.png';
-import notDisturb from '../assets/notDisturb.png';
-
-const SmallAvatar = styled(Avatar)(() => ({
-  width: 17,
-  height: 17,
-  backgroundColor: '#2F3136',
-  border: `3px solid #2F3136`,
-}));
 
 const StatusIcon = ({badge, currentStatus, image, alt}) => {
-  const mapStatus = [offline, online, idle, notDisturb];
-
+  const mapStatus = ['offline', 'online', 'idle', 'notDisturb'];
   return (
     <>
     {
@@ -24,13 +10,15 @@ const StatusIcon = ({badge, currentStatus, image, alt}) => {
         overlap="circular"
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         badgeContent={
-          <SmallAvatar alt="Remy Sharp" src={ mapStatus[currentStatus] } height="10" />
+          <div className='icon-backgound'>
+            <div className={`icon ${mapStatus[currentStatus]}`}></div>
+          </div>
         }
       >
         <Avatar sx={{ height: 32, width: 32 }} src={image} alt={ alt } />
       </Badge>
       :
-      <Avatar sx={{ height: 10, width: 10 }} src={ mapStatus[currentStatus] } alt={ alt } height='20'/>
+      <div className={`icon ${mapStatus[currentStatus]}`}></div>
     }
       
     </>
